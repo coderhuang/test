e64792b259f9c9a9080128e8a7015ac8df31f3fd
huangzhongjun@sisyphe.com.cn <huangzhongjun@sisyphe.com.cn>
2020-08-19 15:28:43 +0800

    另一种应用启动配置方式

--- jwt-test/src/main/java/toby/jwt/Application.java
+++ jwt-test/src/main/java/toby/jwt/Application.java
@@ -1,19 +1,19 @@
 package toby.jwt;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import toby.jwt.config.filter.JWTFilter;
 
 @SpringBootApplication
-@ServletComponentScan(basePackageClasses = {
-		JWTFilter.class
-})
+@ServletComponentScan(basePackageClasses = { JWTFilter.class })
 public class Application {
 
 	public static void main(String... args) {
 
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder().sources(Application.class).web(WebApplicationType.SERVLET).run(args);
 	}
 }
